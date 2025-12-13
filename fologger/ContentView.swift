3a9fdf1b45b8788df35f173d81b0bf5ffa3d365b
geopeper <geopeper@users.noreply.github.com>
2025-12-13 13:56:56 +0800

    Initial Commit

---
+++ fologger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fologger
+//
+//  Created by 蘇昱齊 on 2025/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fologger/fologgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fologgerApp.swift
+//  fologger
+//
+//  Created by 蘇昱齊 on 2025/12/13.
+//
+
+import SwiftUI
+
+@main
+struct fologgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
